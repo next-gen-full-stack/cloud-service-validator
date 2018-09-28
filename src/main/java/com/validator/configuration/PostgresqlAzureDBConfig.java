15c4655bab6797f7e5366343e58ca3e16bef8ecd
Jerry <ruizdl@cn.ibm.com>
2018-09-28 14:28:27 +0800

    update to display UI

--- src/main/java/com/validator/configuration/PostgresqlAzureDBConfig.java
+++
@@ -1,8 +0,0 @@
-package com.validator.configuration;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationProperties(prefix = "spring.ds.azure.post")
-public class PostgresqlAzureDBConfig extends DBConfig {}
--- src/main/java/com/validator/controllers/api/DBValidatorController.java
+++ src/main/java/com/validator/controllers/api/DBValidatorController.java
@@ -5,8 +5,13 @@ import com.validator.application.usecase.MysqlAliDBOpUsecase;
 import com.validator.application.usecase.MysqlAzureDBOpUsecase;
 import com.validator.application.usecase.PostgresqlAliDBOpUsecase;
 import com.validator.application.usecase.PostgresqlAzureDBOpUsecase;
+import com.validator.beans.DBValidationResult;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +27,14 @@ public class DBValidatorController {
   @Autowired PostgresqlAzureDBOpUsecase postgresqlAzureDBOpUsecase;
 
   @Autowired MysqlAzureDBOpUsecase mysqlAzureDBOpUsecase;
+
+  @Autowired DBValidationResult mysqlAliDBValidationResult;
+
+  @Autowired DBValidationResult postgresqlAliDBValidationResult;
+
+  @Autowired DBValidationResult mysqlAzureDBValidationResult;
+
+  @Autowired DBValidationResult postgresqlAzureDBValidationResult;
 
   @GetMapping(path = "/mysql-ali/all")
   public @ResponseBody Iterable<QRCodeInfo> getAllUsersFromMysqlAliDB() {
@@ -75,5 +88,67 @@ public class DBValidatorController {
       @RequestParam(value = "id") String auto_navi_id) {
     // This returns a JSON or XML with the users
     return postgresqlAzureDBOpUsecase.searchById(auto_navi_id);
+  }
+
+  @RequestMapping("/api/v1/ping/azure/mysql")
+  public DBValidationResult mysqlAzureDBValidation(HttpServletRequest request) {
+    LocalDateTime startTime = LocalDateTime.now();
+    mysqlAzureDBOpUsecase.findAll();
+    LocalDateTime endTime = LocalDateTime.now();
+    // count seconds of the query
+    Duration duration = Duration.between(startTime, endTime);
+    mysqlAzureDBValidationResult.setResponseTime(duration.toMillis());
+    return this.mysqlAzureDBValidationResult;
+  }
+
+  @RequestMapping("/api/v1/ping/azure/postgresql")
+  public DBValidationResult postgresqlAzureDBValidation(HttpServletRequest request) {
+
+    try {
+      LocalDateTime startTime = LocalDateTime.now();
+      postgresqlAzureDBOpUsecase.findAll();
+      LocalDateTime endTime = LocalDateTime.now();
+      // count seconds of the query
+      Duration duration = Duration.between(startTime, endTime);
+      postgresqlAzureDBValidationResult.setResponseTime(duration.toMillis());
+    } catch (Exception e) {
+      postgresqlAzureDBValidationResult.setAccessibility(false);
+      postgresqlAzureDBValidationResult.setScalability(false);
+    }
+
+    return this.postgresqlAzureDBValidationResult;
+  }
+
+  @RequestMapping("/api/v1/ping/ali/mysql")
+  public DBValidationResult mysqlAliDBValidation(HttpServletRequest request) {
+    try {
+      LocalDateTime startTime = LocalDateTime.now();
+      mysqlAliDBOpUsecase.findAll();
+      LocalDateTime endTime = LocalDateTime.now();
+      // count seconds of the query
+      Duration duration = Duration.between(startTime, endTime);
+      mysqlAliDBValidationResult.setResponseTime(duration.toMillis());
+    } catch (Exception e) {
+      mysqlAliDBValidationResult.setAccessibility(false);
+      mysqlAliDBValidationResult.setScalability(false);
+    }
+    return this.mysqlAliDBValidationResult;
+  }
+
+  @RequestMapping("/api/v1/ping/ali/postgresql")
+  public DBValidationResult postgresqlAliDBValidation(HttpServletRequest request) {
+    try {
+      LocalDateTime startTime = LocalDateTime.now();
+      postgresqlAliDBOpUsecase.findAll();
+      LocalDateTime endTime = LocalDateTime.now();
+      // count seconds of the query
+      Duration duration = Duration.between(startTime, endTime);
+      postgresqlAliDBValidationResult.setResponseTime(duration.toMillis());
+    } catch (Exception e) {
+      postgresqlAliDBValidationResult.setAccessibility(false);
+      postgresqlAliDBValidationResult.setScalability(false);
+    }
+
+    return this.postgresqlAliDBValidationResult;
   }
 }
